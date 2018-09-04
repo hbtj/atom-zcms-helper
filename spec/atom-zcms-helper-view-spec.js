dbb2e61c971b15f48184d1e8ea08112f1b4d90fd
wangzhaohui <alex.wang@zving.com>
2018-09-04 10:10:55 +0800

    first commit

---
+++ spec/atom-zcms-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomZcmsHelperView from '../lib/atom-zcms-helper-view';
+
+describe('AtomZcmsHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
